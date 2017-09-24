692572036607ea237f1caffe30cdeaefd46bdaf6
doirss <doriskwa@buffalo.edu>
2017-09-24 16:46:34 -0400

    Created a example petition page!

--- react-backend/client/src/Main.js
+++ react-backend/client/src/Main.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
-
+import ExamplePetition from './ExamplePetition';
 import CreatePetition from './CreatePetition';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Home}/>
+      <Route path='/example' component={ExamplePetition}/>
       <Route path='/create' component={CreatePetition}/>
     </Switch>
   </main>
