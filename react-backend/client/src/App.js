23cc182346f1494578183171dae0c510405fe5f7
Katie <knjames@buffalo.edu>
2017-09-24 03:27:19 -0400

    Added navbar to app with sign in and log in buttons.

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import PetitionList from './PetitionList';
+import Navigation from './Navigation';
 
 const App = () => {
   return (
     <div className="App">
+      <Navigation/>
       <PetitionList/>
     </div>
   );
