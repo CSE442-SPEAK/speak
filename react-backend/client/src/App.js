7d2f57a5452f3ae83668ef39d482b7f7a2a9f7ed
Katie <knjames@buffalo.edu>
2017-09-24 02:50:24 -0400

    Updated App.js to display PetitionList

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,39 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
+import PetitionList from './PetitionList';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <PetitionList/>
+    </div>
+  );
+};
 
 export default App;
