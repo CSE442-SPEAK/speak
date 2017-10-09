3cb98539345100819b3d904a389e24b35af0f7ad
Wendy Shi <wenxuans@buffalo.edu>
2017-10-09 14:32:53 -0400

    adding functionality for creating petitions

--- react-backend/client/src/CreatePetition.js
+++ react-backend/client/src/CreatePetition.js
@@ -7,7 +7,35 @@ import MinimumForm from './MinimumForm';
 import CreateButton from './CreateButton';
 
 class CreatePetition extends Component {
-
+    
+  constructor(props) {
+      super(props);
+      this.handleTitle = this.handleTitle.bind(this);
+      this.handleDescription = this.handleDescription.bind(this);
+      this.state = {
+          title: "",
+          description: "",
+      };
+  }
+  
+  handleTitle(event) {
+      this.setState({
+        title: event.target.value });
+  }
+  
+  handleDescription(event) {
+      this.setState({
+        description: event.target.value });
+  }
+  
+  addPetition = event => {
+      event.preventDefault();
+      this.setState({
+          title: event.target.value,
+          description: event.target.value,
+      });
+  }
+  
   render() {
     return (
       <div className="CreatePetition">
