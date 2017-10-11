10ce3fbbaf50a9b30ef2e7c31be36c2a2e6abba3
Katie <knjames@buffalo.edu>
2017-10-10 22:51:15 -0400

    Editing dbconnection.js

--- speak_api/dbconnection.js
+++ speak_api/dbconnection.js
@@ -1,10 +1,13 @@
 var mysql=require('mysql');
-var connection=mysql.createPool({
+var connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'speak'
+});
 
-host:'localhost',
-user:'root',
-password:'',
-database:'speak'
-
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
 module.exports=connection;
