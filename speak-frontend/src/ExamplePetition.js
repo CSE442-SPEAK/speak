d07e30509213658b70cec4e2e56a05c399de60fc
Wendy Shi <wenxuans@buffalo.edu>
2017-10-11 05:55:43 -0400

    Working on get request for displaying petition

--- speak-frontend/src/ExamplePetition.js
+++ speak-frontend/src/ExamplePetition.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { FormGroup } from 'react-bootstrap';
 import SignButton from './SignButton';
 import './ExamplePetition.css';
 
 class ExamplePetition extends Component {
+    
+  constructor(props) {
+      super(props);
+      this.state = {
+          title: "",
+          description: "",
+      };
+      this.componentDidMount = this.componentDidMount.bind(this);
 
+  }
+  
+  componentDidMount() {
+      fetch('/Petitions/1')
+      .then( function(response) {
+          window.alert(JSON.stringify(response));
+      });
+/*      .then( response => response.json())
+      .then( data => {
+          this.setState({
+              title: data.title,
+              description: data.description,
+          });
+      });*/
+  };
+  
   render() {
+    console.log("hi")
     return (
-      <div className="SignPetition">
+      <div className="ExamplePetition">
         <FormGroup>
           <h1>  
-          New Parking Garage
-	  </h1>
-	  <p>
-	     Petition to create a parking garage at UB North Campus instead of a new stadium!
-	  </p>
+          Example Petition
+          </h1>
+          <h2>
+          {this.state.title}
+          {this.state.description}
+          </h2>
           <SignButton/>
         </FormGroup>
       </div>
