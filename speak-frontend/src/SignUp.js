ca40e8bbb64365316929a2182454fe9778fc008e
Wendy Shi <wenxuans@buffalo.edu>
2017-10-10 23:36:51 -0400

    added major and ubit fields to sign up page

--- speak-frontend/src/SignUp.js
+++ speak-frontend/src/SignUp.js
@@ -25,7 +25,7 @@ class SignUp extends Component {
             <Row className="name">
                 <Col md={6} mdPush={6}>
                   <FieldGroup
-                    id="formControlsText"
+                    id="formControlsLastName"
                     type="text"
                     label="Last Name"
                     placeholder="Enter your last name"
@@ -33,25 +33,43 @@ class SignUp extends Component {
                 </Col>
                 <Col md={6} mdPull={6}>
                   <FieldGroup
-                    id="formControlsText"
+                    id="formControlsFirstName"
                     type="text"
                     label="First Name"
                     placeholder="Enter your first name"
                   />
                 </Col>
             </Row>
-          <FieldGroup
-            id="formControlsEmail"
-            type="email"
-            label="Email Address"
-            placeholder="Enter email"
-          />
-          <FieldGroup
-            id="formControlsPassword"
-            label="Password"
-            type="password"
-          />
-
+            <Row className="schoolInfo">
+                <Col md={6} mdPush={6}>
+                  <FieldGroup
+                    id="formControlsMajor"
+                    label="Major"
+                    type="major"
+                    placeholder="Enter your academic major"
+                  />
+                </Col>
+                <Col md={6} mdPull={6}>
+                  <FieldGroup
+                    id="formControlsUBIT"
+                    label="UBIT"
+                    type="UBIT"
+                    placeholder="Enter your UBIT number"
+                  />
+                </Col>
+            </Row>            
+            <FieldGroup
+                id="formControlsEmail"
+                type="email"
+                label="Email Address"
+                placeholder="Enter email"
+            />
+            <FieldGroup
+                id="formControlsPassword"
+                label="Password"
+                type="password"
+            />
+          
           <Button type="submit">
             Submit
           </Button>
