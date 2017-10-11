7a015921fbde0d33a2fbb0221943859330158f27
Wendy Shi <wenxuans@buffalo.edu>
2017-10-10 22:59:52 -0400

    fleshed out sign up page

--- speak-frontend/src/SignUp.js
+++ speak-frontend/src/SignUp.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
 import './SignUp.css';
-import { FormGroup } from 'react-bootstrap';
+import { Grid, FormGroup, Form, Col, Row, ControlLabel, FormControl, Checkbox, Button, HelpBlock } from 'react-bootstrap';
 import TitleForm from './TitleForm';
 import DescriptionForm from './DescriptionForm';
 import MinimumForm from './MinimumForm';
 
+function FieldGroup({ id, label, help, ...props }) {
+    return (
+        <FormGroup controlId={id}>
+        <ControlLabel>{label}</ControlLabel>
+        <FormControl {...props} />
+        {help && <HelpBlock>{help}</HelpBlock>}
+        </FormGroup>
+    );
+}
+
 class SignUp extends Component {
-    
+  
   render() {
     return (
       <div className="SignUp">
-        <FormGroup>
-          <TitleForm/>
-          <DescriptionForm/>
-          <MinimumForm/>
-        </FormGroup>
+        <Grid>
+        <form>
+            <Row className="name">
+                <Col md={6} mdPush={6}>
+                  <FieldGroup
+                    id="formControlsText"
+                    type="text"
+                    label="Last Name"
+                    placeholder="Enter your last name"
+                  />
+                </Col>
+                <Col md={6} mdPull={6}>
+                  <FieldGroup
+                    id="formControlsText"
+                    type="text"
+                    label="First Name"
+                    placeholder="Enter your first name"
+                  />
+                </Col>
+            </Row>
+          <FieldGroup
+            id="formControlsEmail"
+            type="email"
+            label="Email Address"
+            placeholder="Enter email"
+          />
+          <FieldGroup
+            id="formControlsPassword"
+            label="Password"
+            type="password"
+          />
+
+          <Button type="submit">
+            Submit
+          </Button>
+        </form>
+        </Grid>
       </div>
     );
   }
