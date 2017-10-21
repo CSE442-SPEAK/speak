addcbcd53c25b02d8bab0eccbe79541da15ad13a
Katie <knjames@buffalo.edu>
2017-10-21 18:30:14 -0400

    Changed port to 8080.

--- speak_api/app.js
+++ speak_api/app.js
@@ -51,7 +51,7 @@ if (app.get('env') === 'development') {
 }
 
 if (module === require.main) {
-    const server = app.listen(process.env.PORT || 3001, () => {
+    const server = app.listen(process.env.PORT || 8080, () => {
         const port = server.address().port;
         console.log(`App listening on port ${port}`);
     })}
