426a01e30bfce198af494c97594427fcff65d2ad
Heeba Kariapper <hkariapper@gmail.com>
2017-11-15 02:05:50 -0500

    some updates, fixing first part of what crashes

--- speak-frontend/src/PetitionListElement.js
+++ speak-frontend/src/PetitionListElement.js
@@ -31,9 +31,9 @@ class PetitionListElement extends Component {
         <Card>
           <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+            <CardTitle>Petition title</CardTitle>
+            <CardSubtitle>Petition subtitle</CardSubtitle>
+            <CardText>Petition Description</CardText>
             <Button>Button</Button>
           </CardBody>
         </Card>
