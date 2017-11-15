20dcfa32396032f2dd5996641170c3ccce85ffb8
Heeba Kariapper <hkariapper@gmail.com>
2017-11-15 02:03:50 -0500

    card view starting to integrate, need to include calls to petition

--- speak-frontend/src/PetitionListElement.js
+++ speak-frontend/src/PetitionListElement.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import { Panel, Button } from 'react-bootstrap';
 
+import {
+  Card,
+  CardBlock,
+  CardFooter,
+  CardTitle,
+  CardText,
+  CardImg,
+  CardBody,
+  CardSubtitle
+} from 'react-bootstrap-card';
+import {
+  Col,
+  Row,
+} from 'react-bootstrap';
+
+import PropTypes from 'prop-types';
+
 class PetitionListElement extends Component {
-    
+
   constructor(props) {
       super(props)
   }
-  
+
   render() {
-    
+
     return (
-      <div className="PetitionListElement">
-        <Panel collapsible header={this.props.title}>
-          <p>{this.props.description}</p>
-          <div className="LearnMoreButton">
-            <Button bsStyle="success" href={'/petitions/'+this.props.id}>Learn more</Button>
-          </div>
-        </Panel>
+      <div>
+        <Card>
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
       </div>
     );
   }
