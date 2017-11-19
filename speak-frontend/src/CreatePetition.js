2f7fed28d190b1cd9934541c1794c490b3e69b87
Wendy Shi <wenxuans@buffalo.edu>
2017-11-18 21:12:57 -0500

    Added authentication check to Create Petition and Sign Petition

--- speak-frontend/src/CreatePetition.js
+++ speak-frontend/src/CreatePetition.js
@@ -65,51 +65,76 @@ class CreatePetition extends Component {
 
   }
 
+  login() {
+    this.props.auth.login();
+  }
+
   render() {
+    const { isAuthenticated } = this.props.auth;
     return (
-      <div className="CreatePetition">
-        <h1> Create a Petition </h1>
-        <Grid>
-            <Row classname="show-grid">
-                <Col xs={12} md={8} xsOffset={2}>
-                    <FormGroup>
-                        <div className="TitleForm">
-                            <ControlLabel>Petition Title</ControlLabel>
-                            <FormControl
-                              type="text"
-                              placeholder="Title"
-                              bsSize="large"
-                              onChange={this.handleTitle}
-                            />
-                            <FormControl.Feedback />
-                        </div>
-                        <div className="DescriptionForm">
-                            <ControlLabel>Petition description</ControlLabel>
-                            <FormControl
-                                componentClass="textarea"
-                                placeholder="Description"
-                                bsSize="large"
-                                onChange={this.handleDescription}
-                            />
-                        </div>
-                        <div className="MinimumForm">
-                            <ControlLabel>Minimum number of signatures required</ControlLabel>
-                            <FormControl
-                              type="text"
-                              placeholder="Integer value"
-                              bsSize="large"
-                              onChange={this.handleSignatureGoal}
-                            />
-                        </div>
-                        <Button classname="createPetition"
-                            type="submit"
-                            onClick={this.addPetition}
-                            bsStyle="success"
-                        > Submit Your Petition </Button>
-                    </FormGroup>
-                </Col>
-            </Row>
-        </Grid>
+      <div classname="container">
+      {
+        isAuthenticated() && (
+          <div className="CreatePetition">
+            <h1> Create a Petition </h1>
+            <Grid>
+                <Row classname="show-grid">
+                    <Col xs={12} md={8} xsOffset={2}>
+                        <FormGroup>
+                            <div className="TitleForm">
+                                <ControlLabel>Petition Title</ControlLabel>
+                                <FormControl
+                                  type="text"
+                                  placeholder="Title"
+                                  bsSize="large"
+                                  onChange={this.handleTitle}
+                                />
+                                <FormControl.Feedback />
+                            </div>
+                            <div className="DescriptionForm">
+                                <ControlLabel>Petition description</ControlLabel>
+                                <FormControl
+                                    componentClass="textarea"
+                                    placeholder="Description"
+                                    bsSize="large"
+                                    onChange={this.handleDescription}
+                                />
+                            </div>
+                            <div className="MinimumForm">
+                                <ControlLabel>Minimum number of signatures required</ControlLabel>
+                                <FormControl
+                                  type="text"
+                                  placeholder="Integer value"
+                                  bsSize="large"
+                                  onChange={this.handleSignatureGoal}
+                                />
+                            </div>
+                            <Button classname="createPetition"
+                                type="submit"
+                                onClick={this.addPetition}
+                                bsStyle="success"
+                            > Submit Your Petition </Button>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </Grid>
+          </div>
+        )
+      }
+      {
+        !isAuthenticated() && (
+          <h4>
+            You are not logged in! Please{' '}
+            <a
+              style={{ cursor: 'pointer' }}
+              onClick={this.login.bind(this)}
+            >
+            Log In
+            </a>
+            {' '}to continue.
+          </h4>
+        )
+      }
       </div>
     );
   }
