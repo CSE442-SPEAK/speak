6130311a3b13b7a6ab87f6a6f605d24c34c39709
Wendy Shi <wenxuans@buffalo.edu>
2017-11-18 19:48:01 -0500

    Log in and log out is functional

--- speak-frontend/src/Navigation.js
+++ speak-frontend/src/Navigation.js
@@ -10,7 +10,29 @@ class Navigation extends Component {
 
   componentWillMount() {
     this.lock = new Auth0Lock('3AENWl_-dFQnyEOBAlq7AMMhi_K7RUwy', 'speak-ub.auth0.com');
+    //this.setState({idToken: this.getIdToken()})
   }
+
+  /*
+  createLock() {
+    this.lock = new Auth0Lock(this.props.clientId, this.props.domain);
+  }
+
+  getIdToken() {
+    var idToken = localStorage.getItem('id_token');
+    var authHash = this.lock.parseHash(window.location.hash);
+    if (!idToken && authHash) {
+      if (authHash.id_token) {
+        idToken = authHash.id_token;
+        localStorage.setItem('id_token', authHash.id_token);
+      }
+      if (authHash.error) {
+        console.log("Error signing in", authHash);
+      }
+    }
+    return idToken;
+  }
+  */
 
   render() {
     return (
--- speak-frontend/src/index.js
+++ speak-frontend/src/index.js
@@ -1,16 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { createRoutes } from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  ), document.getElementById('root')
-  );
+const routes = createRoutes();
+
+ReactDOM.render(
+  routes,
+  document.getElementById('root')
+);
 registerServiceWorker();
