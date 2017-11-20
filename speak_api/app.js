d2107465e49bf5b6b2383550dc9ae41686e50c00
Katie <knjames@buffalo.edu>
2017-11-20 01:17:15 -0500

    Updating app.js for deployment.

--- speak_api/app.js
+++ speak_api/app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const config = require('./config');
 const app = express();
-/**const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
-const jwksRsa = require('jwks-rsa');*/
 const cors = require('cors');
 const morgan = require('morgan');
 var Users = require('./routes/Users');
@@ -18,39 +15,6 @@ if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
 app.use(cors());
 app.use(morgan('API Request (port 3001): :method :url :status :response-time ms - :res[content-length]'));
 
-app.use('/Users', Users);
-app.use('/Petitions', Petitions);
-app.use('/Signatures', Signatures);
-
-app.listen(3001);
-console.log('Server listening on http://localhost:3001.')
-
-/**==========================================================
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var routes = require('./routes/index');
-var Users = require('./routes/Users');
-var Petitions = require('./routes/Petitions');
-var Signatures = require('./routes/Signatures');
-const config = require('./config');
-const app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(cors());
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended:false }));
-
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/',routes);
 app.use('/Users', Users);
 app.use('/Petitions', Petitions);
 app.use('/Signatures', Signatures);
