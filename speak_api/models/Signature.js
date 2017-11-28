f11f7a074c7d150bfa925144a1f2c3b4fa1d109c
Katie <knjames@buffalo.edu>
2017-11-27 22:15:47 -0500

    Changing how we add a user into the database when signing a petition if they don't already exist.

--- speak_api/models/Signature.js
+++ speak_api/models/Signature.js
@@ -30,19 +30,19 @@ getSignatureById:function(signature_id, callback) {
 },
 
 addSignature:function(Signature, callback) {
-  db.query("select * from user where email=?", [Signature.email], function(err, result){
+  return db.query("select * from user where email=?", [Signature.email], function(err, result){
     if(result.length == 0) {
       db.query("Insert into user(email) values(?)", [Signature.email], callback);
     }
+    db.query("select * from signature where petition_id=? and user_id=(select user_id from user where email=?)", [Signature.petition_id, Signature.email], function(err, result){
+      if (result.length == 0) {
+        db.query("update petition set signatures = signatures + 1 where petition_id=?", [Signature.petition_id], callback);
+        return db.query("Insert into signature(petition_id, user_id, date) values(?,(select user_id from user where email=?),?)", [Signature.petition_id, Signature.email, Signature.date], callback);
+      } else {
+        return "User has already signed this petition.";
+      }
   });
-  return db.query("select * from signature where petition_id=? and user_id=(select user_id from user where email=?)", [Signature.petition_id, Signature.email], function(err, result){
-    if (result.length == 0) {
-      db.query("update petition set signatures = signatures + 1 where petition_id=?", [Signature.petition_id], callback);
-      return db.query("Insert into signature(petition_id, user_id, date) values(?,(select user_id from user where email=?),?)", [Signature.petition_id, Signature.email, Signature.date], callback);
-    } else {
-      return "User has already signed this petition.";
-    }
-  });
+});
 },
 
 deleteSignature:function(signature_id, callback){
